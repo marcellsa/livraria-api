2a0829c69d1e7422e29f1da7903682f43ca31fb8
Marcel Albuquerque <msalbuquerque@hotmail.com>
2023-09-20 17:24:00 -0300

    chore: configurações e intalando dependências para iniciar TDD no projeto

---
+++ src/test/livros.test.js
@@ -0,0 +1,49 @@
+import crypto from "crypto";
+import axios from "axios";
+import livroController from "../controllers/livroController.js";
+
+// Gerar uma string aleatória de comprimento específico
+const generateRandomString = (length) => {
+  const bytes = crypto.randomBytes(Math.ceil(length / 2));
+  return bytes.toString("hex").slice(0, length);
+};
+
+// Gerar um número de ponto flutuante aleatório entre um mínimo e um máximo
+const generateRandomFloat = (min, max) => {
+  return Math.random() * (max - min + min);
+};
+
+// Gerar um número aleatório entre um mínimo e máximo
+const generateRandomNumber = (min, max) => {
+  const range = max - min + 1;
+  return Math.floor(Math.random() * range) + min;
+};
+
+// Envia uma solicitação HTTP usando a biblioteca Axios
+const request = (url, method, data) => axios({ url, method, data });
+
+describe("Testando rota livros", () => {
+
+  test("Deve retornar livros", async() => {
+    //given - dado que
+    const livro1 = await livroController.cadastrarLivro({
+      titulo: generateRandomString(10), 
+      editora: generateRandomString(10), 
+      preco: generateRandomFloat(0,100), 
+      paginas: generateRandomNumber(99,500)
+    });
+    const livro2 = await livroController.cadastrarLivro({
+      titulo: generateRandomString(10), 
+      editora: generateRandomString(10), 
+      preco: generateRandomFloat(0,100), 
+      paginas: generateRandomNumber(99,500)
+    });
+
+    // when - quando
+    const response = await request("http://localhost:3000/livros", "get");
+    const livros = response.data;
+
+    // then - então
+    expect(livros).toHaveLength(3);
+  });
+});
