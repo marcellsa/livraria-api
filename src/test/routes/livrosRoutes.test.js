b2ce7b13b4e71467bec958d4029070ffcff0d491
Marcel Albuquerque <msalbuquerque@hotmail.com>
2023-10-05 17:34:47 -0300

    test(routes): implementnando testes para rota livros (POST)

--- src/test/routes/livrosRoutes.test.js
+++ src/test/routes/livrosRoutes.test.js
@@ -16,9 +16,25 @@ describe("Testando rota livros", () => {
 
   test("Deve retornar a lista de livros", async () => {
     
-    await request(app)
+    const response = await request(app)
       .get("/livros")
       .expect(200);
 
+    expect(response.status).toEqual(200);
+
+  });
+
+  test("Deve cadastrar o livro quando os dados enviados forem válidos", async () => {
+    
+    const response = await request(app)
+      .post("/livros")
+      .send({
+        titulo: "O Senhor dos Anéis 2",
+        editora: "Allen & Unwin",
+        paginas: 1000
+      })
+      .expect(201);
+
+    expect(response.status).toEqual(201);
   });
 });
