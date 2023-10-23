a0fdd80837403da3f28101b1b5c70082360e6e06
Marcel Albuquerque <msalbuquerque@hotmail.com>
2023-10-23 17:41:27 -0300

    feat(contr): implementando método listarLivroPorId

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -4,8 +4,22 @@ class LivroController {
 
   // métodos estáticos assim não há obrigação de criar uma instância prévia da classe
   static async listarLivros (req, res) {
-    const listaDeLivros =  await livro.find({});
-    res.status(200).json(listaDeLivros);
+    try {
+      const listaDeLivros =  await livro.find({});
+      res.status(200).json(listaDeLivros);
+    }  catch (erro) {
+      res.status(500).json({ message: `${erro.message} - falha na requisição` });
+    }
+  }
+
+  static async listarLivroPorId (req, res) {
+    try {
+      const { id } = req.params;
+      const livroEncontrado = await livro.findById(id);
+      res.status(200).json(livroEncontrado);
+    } catch (erro) {
+      res.status(500).json({ message: `${erro.message} - falha na requisição do livro` });
+    }
   }
 
   static async cadastrarLivro (req, res) {
@@ -13,7 +27,7 @@ class LivroController {
       const novoLivro = await livro.create(req.body);
       res.status(201).json({ message: "Livro cadastrado com sucesso", livro: novoLivro });
     } catch (erro) {
-      res.status(500).json({ message: `${erro.message} - falha ao cadastrar livro`});
+      res.status(500).json({ message: `${erro.message} - falha ao cadastrar livro` });
     }
   }
 
